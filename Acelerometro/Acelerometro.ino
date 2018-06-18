8c0bd4f1b8bf4d0016b7b03bc838c63017cd7cf9
Daniel <daniel_arod@protonmail.com>
2018-06-17 19:27:52 -0500

    Version 3.0

---
+++ Acelerometro/Acelerometro.ino
@@ -0,0 +1,30 @@
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+
+MPU6050 mpu6050(Wire);
+
+long timer = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+}
+
+void loop() {
+  mpu6050.update();
+ 
+    Serial.print("accX : ");Serial.print(mpu6050.getAccX());
+    Serial.print("\taccY : ");Serial.print(mpu6050.getAccY());
+    Serial.print("\taccZ : ");Serial.println(mpu6050.getAccZ());
+
+  
+   
+   
+   
+    
+    
+  
+
+}
