95f9284ec781b77f6471d19a3bf45946a0d9f37d
Daniel <daniel_arod@protonmail.com>
2018-07-25 13:46:16 -0500

    Version 3.6

--- Sensores_Separados/Sensores_Separados.ino
+++ Sensores_Separados/Sensores_Separados.ino
@@ -1,99 +1,73 @@
-// Version 1.01
+/*
+    MPU6050 Triple Axis Gyroscope & Accelerometer. Pitch & Roll & Yaw Gyroscope Example.
+    Read more: http://www.jarzebski.pl/arduino/czujniki-i-sensory/3-osiowy-zyroskop-i-akcelerometr-mpu6050.html
+    GIT: https://github.com/jarzebski/Arduino-MPU6050
+    Web: http://www.jarzebski.pl
+    (c) 2014 by Korneliusz Jarzebski
+*/
 
-//Librerias 
-#include <Wire.h>//Lib comun 
-#include <MPU6050_tockn.h>//Acelerometro y giroscopio, hecha por un genio
-#include <Adafruit_BMP280.h>//Barometro
-#include <Adafruit_Sensor.h>//Barometro
+#include <Wire.h>
+#include <MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
 
-//Variables 
-//Acelerometro/Giroscopio
-MPU6050 mpu6050(Wire);
-//Barometro
-float valores_n = 0;  //Es la altura que mide el sensor en un instante k, valores sin refinar
-float altura_ref = 0; //se hace una medicion y se pone como valor negativo, de tal manera que la diferencia entre los futuros datos
-                      // y la altura actual sea la correcta, si no se mueve se mantiene en cero
+MPU6050 mpu;
 
-float valores_s = 0; //Son los valores corregidos
-float altura_maxima = 0;
- 
-float presion=560; //la presion local, bogota esta fucking arriba
-Adafruit_BMP280 bme;//I2C
+// Timers
+unsigned long timer = 0;
+float timeStep = 0.01;
 
-//Funciones
-//Barometro
-void analizar_altura();//amplia la señal inicial para que las opperaciones sean mas sencillas
-void tierra();//corrige los datos negativos igualandolos a cero
-float correccion();//devuelve los datos corregidos, util para hacer operaciones
+// Pitch, Roll and Yaw values
+float pitch = 0;
+float roll = 0;
+float yaw = 0;
 
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11 
+#define BMP_CS 10
 
+Adafruit_BMP280 bmp; // I2C
 
-void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-  //Acelerometro/Giroscopio
-  mpu6050.begin();
-  mpu6050.calcGyroOffsets(true);
-  //Barometro
-  if(!bme.begin()){
-    Serial.println("error");
-  } delay(100);//una pequeña espera para darle tiempo de iniciar
-  altura_ref=bme.readAltitude(presion);//la diferencia para que el valor inicial sea cero
-  
-}
-
-void loop() {
-  mpu6050.update();
-
-  if(!bme.begin()){
-    Serial.println("error Barometro");
-    while(!bme.begin());
+void setup() 
+{
+  Serial.begin(115200);
+   if (!bmp.begin()) {  
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
   }
-  analizar_altura();//toma el dato de la altura actual
-  tierra(); //verifica que el dato no sea menor que cero
-  correccion();//devuelve los datos corregidos
-  
-  if(correccion()>altura_maxima){
-    altura_maxima = correccion();
+  // Initialize MPU6050
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
   }
-  //bastante sencillo no hay mucho misterio, solo toma y muestra los datos
-  Serial.print("Pitch : "); 
-  Serial.print(mpu6050.getAngleX());
-  Serial.print("\tRoll : ");
-  Serial.print(mpu6050.getAngleY());
-  Serial.print("\tYaw : ");
-  Serial.print(mpu6050.getAngleZ());
-  Serial.print("\tBarometro : ");
-  Serial.print(valores_s);
-  Serial.println("\n");
   
-  
-  
+  // Calibrate gyroscope. The calibration must be at rest.
+  // If you don't want calibrate, comment this line.
+  mpu.calibrateGyro();
+
+  // Set threshold sensivty. Default 3.
+  // If you don't want use threshold, comment this line or set 0.
+  mpu.setThreshold(3);
 }
 
-void analizar_altura(){//amplia la señal inicial para que las opperaciones sean mas sencillas
-   valores_n= int((bme.readAltitude(presion)-altura_ref)*100);
-}
+void loop()
+{
+  timer = millis();
 
-void tierra(){ //la gracia de la funcion es que las mediciones, por logica
-  if(valores_n<0){//no pueden ser menores que cero, esto es porque la referencia, osea el suelo
-      valores_n = 0;//siempre sera el cero desde donde se lanza el cohete, no puede ir mas abajo
-    }
-}
+  // Read normalized values
+  Vector norm = mpu.readNormalizeGyro();
 
-float correccion(){ //Es una funcion que devuelve los datos corregidos, bestante util para poder hacer operaciones lugeo
-  int pasado_1 = valores_n;//crea una variable que guarda la altura inmediatamente anterior
-  int correcto_anterior = 0;
+  // Calculate Pitch, Roll and Yaw
+  pitch = pitch + norm.YAxis * timeStep;
+  roll = roll + norm.XAxis * timeStep;
+  yaw = yaw + norm.ZAxis * timeStep;
   
-  if(valores_n-pasado_1>30){//si la diferencia entre la altura actual y la anterior es superior a 30
-      valores_s = (valores_n+pasado_1)/2;//se promedia para equilibrar los datos
-      valores_s = correcto_anterior+ (0.1)*(pasado_1-correcto_anterior);
-      correcto_anterior = valores_s;
-  }else{//si no lo es, los valores correctos corresponderan a los valores medidos
-      valores_s = correcto_anterior+ (0.1)*(pasado_1-correcto_anterior);
-      correcto_anterior = valores_s;
-    }
+  // Output raw
+  mostrarData();
+  
+  
+  // Wait to full timeStep period
+  delay((timeStep*1000) - (millis() - timer));
 }
-
-
-
