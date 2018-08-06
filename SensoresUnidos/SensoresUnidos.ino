42c74e8014ee637d66da854d0a73c2611bde422d
Daniel <daniel_arod@protonmail.com>
2018-08-06 18:16:22 -0500

    Version 3.2

--- SensoresUnidos/SensoresUnidos.ino
+++ SensoresUnidos/SensoresUnidos.ino
@@ -5,49 +5,29 @@ Sensor que se usa : GY-86
 */
 
 #include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
 #include <MPU6050.h>
 #include <MS5611.h>
-#include <SD.h>
-#include <SPI.h>
-#include "I2Cdev.h"
 
+//Variables memoria SD
 File myFile;
 int pinCS = 4; // Pin 4 on Arduino Uno
 
-// Timers
-float timeStep = 0.1;
-
-// Pitch, Roll and Yaw values
-float pitch = 0;
-float roll = 0;
-float yaw = 0;
-
-
-//Variables MS5611
-
-double realTemperature;
-long realPressure;
-float absoluteAltitude;
-float relativeAltitude;
-double referencePressure;
-
+//Declaramos los Sensores
 MPU6050 mpu;
 MS5611 ms5611;
 
-void setup() 
-{
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
-  Serial.begin(9600);
+//Varaibles MS5611
+double referencePressure;
 
-  pinMode(pinCS, OUTPUT);
+void setup()
+{ Serial.begin(115200);
+
+// SD Card Initialization
+  pinMode(pinCS, OUTPUT);  
   
-  // SD Card Initialization
-  if (SD.begin())
+  if (SD.begin()) 
   {
     Serial.println("SD card is ready to use.");
   } else
@@ -56,77 +36,68 @@ void setup()
     return;
   }
   
-  // Initialize MS5611
-  while(!ms5611.begin())
-  {
-    Serial.println("Could not find a valid MS5611 sensor, check wiring!");
-    delay(500);
-  }
-  
   // Initialize MPU6050
   while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
   {
     Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
   }
-   
-   
-   // Get reference pressure for relative altitude
+
+  
+  // Initialize MS5611 sensor
+  Serial.println("Initialize MS5611 Sensor");
+
+  while(!ms5611.begin())
+  {
+    Serial.println("Could not find a valid MS5611 sensor, check wiring!");
+    delay(500);
+  }
+
+  // Get reference pressure for relative altitude
   referencePressure = ms5611.readPressure();
+
   // Check settings
-  checkSettingsMS5611();
-  
-  // Calibrate gyroscope. The calibration must be at rest.
-  // If you don't want calibrate, comment this line.
-  mpu.calibrateGyro();
+  checkSettings();
+   
+}
 
-  // Set threshold sensivty. Default 3.
-  // If you don't want use threshold, comment this line or set 0.
-  mpu.setThreshold(3);
-
-  /*
-  mpu.setAccelPowerOnDelay(MPU6050_DELAY_3MS);
-  
-  mpu.setIntFreeFallEnabled(true);
-  mpu.setIntZeroMotionEnabled(false);
-  mpu.setIntMotionEnabled(false);
-  
-  mpu.setDHPFMode(MPU6050_DHPF_5HZ);
-  mpu.setFreeFallDetectionThreshold(17);
-  mpu.setFreeFallDetectionDuration(2);  
-  
-  checkSettingsMPU6050();
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
-  
-  attachInterrupt(0, doInt, RISING);*/
+void checkSettings()
+{
+  Serial.print("Oversampling: ");
+  Serial.println(ms5611.getOversampling());
 }
 
 
-void loop()
-{  
- //Read normalized values gyro
- Vector norm = mpu.readNormalizeGyro();
- /* lee la aceleracion
-  Vector rawAccel = mpu.readRawAccel();
-  //lee la actividad xd
-  Activites act = mpu.readActivites();
-  */
-  // Calculate Pitch, Roll and Yaw
-  pitch = pitch + norm.YAxis * timeStep;
-  roll = roll + norm.XAxis * timeStep;
-  yaw = yaw + norm.ZAxis * timeStep;
 
-  MS5611Datos();
+void loop()
+{  // Read normalized values 
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  // Calculate Pitch & Roll
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
+  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
+  
+  uint32_t rawTemp = ms5611.readRawTemperature();
+  uint32_t rawPressure = ms5611.readRawPressure();
+
+  // Read true temperature & Pressure
+  double realTemperature = ms5611.readTemperature();
+  long realPressure = ms5611.readPressure();
+
+  // Calculate altitude
+  float absoluteAltitude = ms5611.getAltitude(realPressure);
+  float relativeAltitude = ms5611.getAltitude(realPressure, referencePressure);
+ 
   // Output raw
   String coma = String(',');
   String Pitch = String(pitch);
   String Roll = String(roll);
-  String Yaw = String(yaw);
-  String Temp = String(realTemperature);
-  String Presion = String(realPressure);
-  String Altura = String(relativeAltitude);
-  String datos = String(Pitch+coma+Roll+coma+Yaw+coma+Temp+coma+Presion+coma+Altura);
+  String Xnorm = String(normAccel.XAxis);
+  String Ynorm = String(normAccel.YAxis);
+  String Znorm = String(normAccel.ZAxis);
+  String Altura = String(relativeAltitude); 
+  
+  String datos = String(Pitch+coma+Roll+coma+Xnorm+coma+Ynorm+coma+Znorm+coma+Altura);
   Serial.println(datos);
   
 
@@ -139,20 +110,6 @@ void loop()
   else {
     Serial.println("error opening test.txt");
   }
-
-  /*if (freefallDetected)
-  {
-    ledState = !ledState;
-    digitalWrite(4, ledState);
-    freefallBlinkCount++;
-    if (freefallBlinkCount == 20)
-    {
-      freefallDetected = false;
-      ledState = false;
-      digitalWrite(4, ledState);
-    }
-  }
-  */
 
   // Wait to full timeStep period
   delay(50);
