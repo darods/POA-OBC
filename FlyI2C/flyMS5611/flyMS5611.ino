2c9a73b4effd7cccb2c0f028c25480e98c63607d
NASA <daniel_arod@protonmail.com>
2018-12-22 21:13:21 -0500

    primeros ensayos i2c con el sensor MS5611

--- FlyI2C/flyMS5611/flyMS5611.ino
+++ FlyI2C/flyMS5611/flyMS5611.ino
@@ -5,10 +5,32 @@ long presion, temperatura;
 void setup(){
   Serial.begin(9600);
   Wire.begin();
-  setMS5611();
+  //setMS5611();
 }
 
-setMS5611(){
+void setMS5611(){
    Wire.beginTransmission(0x77); //esta es la direccion encontrada por el sketch y el fabricante
-   Wire.write(); 
+   Wire.write(0x1E);
+   Wire.endTransmission();  
+   Wire.beginTransmission(0x77); //esta es la direccion encontrada por el sketch y el fabricante
+   Wire.write(0x48);
+   Wire.endTransmission();  
+   
 }
+
+void loop(){
+    Wire.beginTransmission(0x77);
+    //Wire.write(0x1E);
+    Wire.endTransmission();
+    Wire.requestFrom(0x46,0);
+    presion = Wire.read() <<8 | Wire.read();
+    //temperatura = Wire.read() <<8 | Wire.read();
+
+    Serial.print("presion = ");
+    Serial.println(presion);
+    //Serial.print("temp = ");
+    //Serial.println(temperatura);
+
+    delay(1000);
+    
+  }
