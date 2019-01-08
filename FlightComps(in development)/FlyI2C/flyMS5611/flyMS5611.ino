40a01d8068aeae2b4641424df35ac4f201a0a6c3
NASA <daniel_arod@protonmail.com>
2019-01-07 21:03:36 -0500

    Added some changes in flMS5611.ino suggested by koepel in the issue

--- FlightComps(in development)/FlyI2C/flyMS5611/flyMS5611.ino
+++ FlightComps(in development)/FlyI2C/flyMS5611/flyMS5611.ino
@@ -1,23 +1,64 @@
+//Guide : https://github.com/ChristofSchmid/Baro-MS5611/blob/master/MS5611-I2C-Test.ino
 #include <Wire.h>
 
 //the addess of the MS5611
-int MS_sensor = 0x77;
+//int MS_sensor = 0x77;
 
 
 void setup(){
   Serial.begin(9600);
   Wire.begin();
+  MS5611SetUp();
   
 }
 
 
 void loop(){
-  //Start request
-  Wire.beginTransmission(MS_sensor);
-  Wire.write(0x42);
-  Wire.endTransmission();
-  Wire.requestFrom(MS_sensor,24);
-  while(Wire.available() == 0); //wait for response
-  Serial.println(Wire.read());
-  delay(3000);  
+    //recordPROM();
+    MS5611SetUp();
+    Serial.println(recordTemp());
+    
+    delay(500);
   }
+
+void MS5611SetUp(){
+    Wire.beginTransmission(0x77);
+    Wire.write(0x1E);//the datasheet says that the first things you have to do after giving power to the arduino is running the "RESET" function
+    //given by the registry 0x1E
+    Wire.endTransmission();
+  }
+
+void recordADC(){
+    Wire.beginTransmission(0x77);
+    Wire.write(0x00);
+    Wire.endTransmission();
+    Wire.requestFrom(0x77,1);
+    //Serial.println(Wire.read());
+  }
+
+void recordPROM(){
+    Wire.beginTransmission(0x77);
+    Wire.write(0xA2);
+    Wire.endTransmission();
+    Wire.requestFrom(0x77,1);
+    //Serial.println(Wire.read());
+  }
+
+uint32_t recordTemp(){
+    uint32_t value;
+    Wire.beginTransmission(0x77);
+    Wire.write(0x50);
+    Wire.endTransmission();
+    Wire.beginTransmission(0x77);
+    Wire.write(0x00);
+    Wire.endTransmission();
+    Wire.requestFrom(0x77,3);
+
+    uint8_t vxa = Wire.read();
+    uint8_t vha = Wire.read();
+    uint8_t vla = Wire.read();
+    
+    value = ((int32_t)vxa << 16) | ((int32_t)vha << 8) | vla;
+
+    return value;
+  }    
